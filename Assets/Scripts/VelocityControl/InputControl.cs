10809c1513c9369a08a8df4b3d9fe01d36047522
Ben <benjamin.jarvis@epfl.ch>
2024-07-25 17:00:26 +0200

    First Changes

--- Assets/Scripts/VelocityControl/InputControl.cs
+++ Assets/Scripts/VelocityControl/InputControl.cs
@@ -15,11 +15,15 @@ public class InputControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-//		vc.desired_vx = Input.GetAxisRaw ("Pitch")*4.0f;
-//		vc.desired_vy = Input.GetAxisRaw ("Roll")*4.0f;
-//		vc.desired_yaw = Input.GetAxisRaw ("Yaw")*0.5f;
-//		abs_height += Input.GetAxisRaw("Throttle") * 0.1f;
+		
+		// Log the pitch, roll, yaw, and throttle inputs
+		Debug.Log("Pitch: " + Input.GetAxis("Pitch") + " Roll: " + Input.GetAxis("Roll") + " Yaw: " + Input.GetAxis("Yaw") + " Throttle: " + Input.GetAxis("Throttle"));
+
+		vc.desired_vx = Input.GetAxisRaw ("Pitch")*4.0f;
+		vc.desired_vy = Input.GetAxisRaw ("Roll")*4.0f;
+		vc.desired_yaw = Input.GetAxisRaw ("Yaw")*0.5f;
+		abs_height += Input.GetAxisRaw("Throttle") * 0.1f;
 //
-//		vc.desired_height = abs_height;
+		vc.desired_height = abs_height;
 	}
 }
