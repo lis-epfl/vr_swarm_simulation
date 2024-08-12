47fb33bfa54e15df748683487007128413bc3d21
bjarvis <benjamin.jarvis@epfl.ch>
2024-08-12 13:15:19 +0200

    Correct Meta/Oculus setup

---
+++ Assets/Scripts/swarm/screenSpawn.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenPawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        // Create a quad object, render texture, and screen material to display the camera of each drone
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
