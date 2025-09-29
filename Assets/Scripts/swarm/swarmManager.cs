03cafbd9d9dd1c50f9692825f7fdf0e6243428b1
AdvaithSriram <sriram.advaith@gmail.com>
2025-09-29 16:43:05 +0200

    Added NBV.cs file: - Made modifications to SwarmAlgorithm and SwarmManager to reflected new NBV option - NBV.cs trial to move swarm around reference point

--- Assets/Scripts/swarm/swarmManager.cs
+++ Assets/Scripts/swarm/swarmManager.cs
@@ -9,7 +9,8 @@ public class SwarmManager : MonoBehaviour
     public enum SwarmAlgorithm
     {
         REYNOLDS,
-        OLFATI_SABER
+        OLFATI_SABER,
+        NBV // NEW ADVAITH NBV
     }
 
     [Header("Swarm Algorithm")]
@@ -34,7 +35,16 @@ public class SwarmManager : MonoBehaviour
     public float lambda_obs = 1.0f;
     public float c_obs = 4.3f;
     public float scaleFactor = 10.0f;
+
     
+    [Header("NBV Parameters")] // NEW ADVAITH NBV
+    public float viewDistance = 10.0f;
+    public float informationGainWeight = 2.0f;
+    // ... any other parameters you need
+    public float radius = 20.0f;
+    public float height = 50.0f;
+    public Vector3 centerPoint = new Vector3(0, 0, 0);
+    public float movementSpeed = 5.0f; // END OF NEW ADVAITH NBV
 
     public enum AttitudeControl
     {
@@ -82,7 +92,7 @@ public class SwarmManager : MonoBehaviour
     public float GetDelta() => delta;
     public float GetA() => a;
     public float GetB() => b;
-    public float GetC() => (b-a) / (2 * Mathf.Sqrt(a*b));
+    public float GetC() => (b - a) / (2 * Mathf.Sqrt(a * b));
     public float GetGamma() => gamma;
     public float GetCVM() => c_vm;
     public float GetDObs() => d_obs;
@@ -94,5 +104,13 @@ public class SwarmManager : MonoBehaviour
     // Getters for the attitude control
     public int GetNumNeighbours() => numNeighbours;
     public int GetNumDimensions() => numDimensions;
+
+    // Getters for NBV parameters // NEW ADVAITH NBV
+    public float GetViewDistance() => viewDistance;
+    public float GetInformationGainWeight() => informationGainWeight;
+    public float GetRadius() => radius;
+    public float GetHeight() => height;
+    public Vector3 GetCenterPoint() => centerPoint;
+    public float GetMovementSpeed() => movementSpeed; // END OF NEW ADVAITH NBV
 
 }
