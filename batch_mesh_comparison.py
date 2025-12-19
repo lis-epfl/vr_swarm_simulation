ad50c3b891f8a54ee6b322c00616d3edae40e566
advaithsriram <sriram.advaith@gmail.com>
2025-12-19 13:57:37 +0100

    nbv automation 3

--- batch_mesh_comparison.py
+++ batch_mesh_comparison.py
@@ -270,8 +270,8 @@ def parse_filename(filename: str) -> tuple:
 def main():
     # Paths
     final_pc_folder = Path("Assets/FinalPointClouds")
-    gt_path = "Assets/Comparisons/House3_GT.obj" # Ground truth mesh path House_with_Texture3.obj original
-    output_csv = "comparison_nbv_house3.csv"
+    gt_path = "Assets/Comparisons/House4_GT.obj" # Ground truth mesh path House_with_Texture3.obj original
+    output_csv = "comparison_nbv_house4.csv"
     
     if not final_pc_folder.exists():
         print(f"Error: Folder not found: {final_pc_folder}")
