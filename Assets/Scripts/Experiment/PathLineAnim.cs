4dee6e4775ce6969a4bd81c49f9607ce1b19a3e0
AlexHeb-LIS <alexandre-1.hebert@outlook.com>
2026-02-13 16:33:17 +0100

    Added a spline to show the centerline to follow in instructions. Also dumped trajectory points of centerline to csv fiel for plotting

--- Assets/Scripts/Experiment/PathLineAnim.cs
+++ Assets/Scripts/Experiment/PathLineAnim.cs
@@ -1,48 +1,99 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Splines;
+using System.IO;
+using System.Globalization;
 
-public class PathLineAnim : MonoBehaviour
+public class MovingDotsOnSpline : MonoBehaviour
 {
+    public SplineContainer spline;
+    public GameObject dotPrefab;
 
-    public Transform[] pathPoints;
-    public LineRenderer line;
-    public float scrollSpeed = 1f;
+    public int dotCount = 15;
+    public float speed = 3f;          // units per second
+    public float spacing = 2f;        // distance between dots
+    public bool exportOnStart = false;
+    public float spatialStep = 0.1f;  // meters between samples
+    public string fileName = "spline_trajectory.csv";
 
+    private Transform[] dots;
+    private float splineLength;
+    private float offsetDistance;
 
-    // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        // line.positionCount = pathPoints.Length;
-        // for (int i = 0; i < pathPoints.Length; i++)
-        //     line.SetPosition(i, pathPoints[i].position + Vector3.up * 0.05f);
-        line.material.mainTexture = MakeDashedTexture();
+        splineLength = spline.CalculateLength();
+        dots = new Transform[dotCount];
+
+        for (int i = 0; i < dotCount; i++)
+        {
+            dots[i] = Instantiate(dotPrefab, transform).transform;
+        }
+        
+        if (exportOnStart)        
+        {
+            Export();
+            exportOnStart = false; // prevent multiple exports if enabled again
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        line.material.mainTextureOffset += new Vector2(Time.deltaTime * scrollSpeed, 0);
+        offsetDistance += speed * Time.deltaTime;
+        offsetDistance %= splineLength;
+
+        for (int i = 0; i < dotCount; i++)
+        {
+            float distance = (i * spacing + offsetDistance) % splineLength;
+
+            float t = spline.Spline.ConvertIndexUnit(
+                distance,
+                PathIndexUnit.Distance,
+                PathIndexUnit.Normalized);
+
+            Vector3 pos = spline.EvaluatePosition(t);
+            Vector3 tangent = spline.EvaluateTangent(t);
+
+            dots[i].position = pos;
+            dots[i].forward = tangent.normalized;
+        }
+    }
+    GameObject CreateDot()
+    {
+        GameObject dot = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        dot.transform.localScale = Vector3.one * 0.2f;
+
+        Destroy(dot.GetComponent<Collider>());
+
+        return dot;
     }
 
-    Texture2D MakeDashedTexture()
+    public void Export()
     {
-        int width = 256;
-        int height = 32;
+        float length = spline.CalculateLength();
+        string path = Path.Combine("Assets/Data", fileName);
 
-        Texture2D tex = new Texture2D(width, height, TextureFormat.RGBA32, false);
-
-        for (int x = 0; x < width; x++)
+        using (StreamWriter writer = new StreamWriter(path))
         {
-            bool dash = (x / 32) % 2 == 0; // dash / gap
-            for (int y = 0; y < height; y++)
+            writer.WriteLine("x,y,z,dx,dy,dz");
+
+            for (float d = 0; d <= length; d += spatialStep)
             {
-                tex.SetPixel(x, y, dash ? Color.white : Color.clear);
+                float t = spline.Spline.ConvertIndexUnit(
+                    d,
+                    PathIndexUnit.Distance,
+                    PathIndexUnit.Normalized);
+
+                Vector3 pos = spline.EvaluatePosition(t);
+                Vector3 tangent = Vector3.Normalize(spline.EvaluateTangent(t));
+
+            writer.WriteLine(
+                string.Format(CultureInfo.InvariantCulture,
+                "{0},{1},{2},{3},{4},{5}",
+                pos.x, pos.y, pos.z,
+                tangent.x, tangent.y, tangent.z));
             }
         }
 
-        tex.Apply();
-        tex.wrapMode = TextureWrapMode.Repeat;
-        return tex;
+        Debug.Log("Spline exported to: " + path);
     }
 }
